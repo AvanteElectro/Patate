f1a58153a9d3dd389c2ee4c6cba1193dde3d870b
AvanteElectro <pinguetlesbabalous@gmail.com>
2022-09-24 22:51:58 +0200

    2ème commit

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < 10; i++)
     {
         std::cout << "Hello world" << std::endl;
         std::this_thread::sleep_for(0.25s);
